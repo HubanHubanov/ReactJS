50b8a928d6a90455632b06dce068df5be87b7187
HubanHubanov <huban.hubanov@gmail.com>
2024-07-04 11:23:16 +0300

    Add html-template

---
+++ 01.Introduction to React and JSX/html-template/src/App.jsx
@@ -0,0 +1,43 @@
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import About from "./components/About";
+import WatchNow from "./components/WatchNow";
+import Features from "./components/Features";
+import OurTeam from "./components/OurTeam";
+import Testimonials from "./components/Testimonials";
+import FAQ from "./components/FAQ";
+import ContactUs from "./components/ContactUs";
+import Download from "./components/Download";
+import Footer from "./components/Footer";
+
+function App() {
+
+  return (
+    <>
+     <Navigation />
+
+      <Header />
+
+      <About />
+
+      <WatchNow />
+
+      <Features />
+
+      <OurTeam />
+
+      <Testimonials />
+
+      <FAQ />
+
+      <ContactUs />
+
+      <Download />
+
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
