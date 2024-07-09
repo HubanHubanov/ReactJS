117e1d435ebd50ed09a98de25f4abc8fde172461
HubanHubanov <huban.hubanov@gmail.com>
2024-07-09 21:19:04 +0300

    Add react-fetch

---
+++ 03.Components Deep Dive/react-fetch/src/App.jsx
@@ -0,0 +1,15 @@
+import CharectersList from './components/CharactersList'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Star Wars</h1>
+
+      <CharectersList />
+    </>
+  )
+}
+
+export default App
