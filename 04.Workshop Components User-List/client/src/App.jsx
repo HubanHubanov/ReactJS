f006982bb731226b35a0e7c22b1d18fdca65b3d4
HubanHubanov <huban.hubanov@gmail.com>
2024-07-16 10:23:45 +0300

    Add workshop components user list

---
+++ 04.Workshop Components User-List/client/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
+import UserList from "./components/user-section/UserSection"
+import "./styles.css"
+
+function App() {
+
+  return (
+    <>
+        <Header />
+        <main className="main">
+            <UserList />
+        </main>
+        <Footer />
+    </>
+  )
+}
+
+export default App
